40500a4b8f78c224561618f6ee7dae7826302ddb
Aidan <aidan.lamos.2020@uni.strath.ac.uk>
2023-11-14 21:01:24 +0000

    Added scroll ability

--- screens/AEDScreen.js
+++ screens/AEDScreen.js
@@ -1,27 +1,45 @@
 import * as React from 'react';
-import { View, Image, Text, StyleSheet, Dimensions } from 'react-native';
+import { View, Image, Text, StyleSheet, Dimensions, ScrollView } from 'react-native';
 import PinIcon from '../components/pin';
 //import externalStyle from '../style/externalStyle';
 
 const screenHeight = Dimensions.get('window').height
+const noOfAED = 7 // placeholder
+
+const scrollViewHeight = ({ noOfAED }) => {
+    return noOfAED % 2 === 1 ? (((noOfAED + 0.5) / 4) * 100).toString() + '%' : (((noOfAED - 0.5)/ 4) * 100).toString() + '%'
+};
+
+const pinHeight = ({ noOfAED }) => {
+    return noOfAED % 2 === 1 ? ((100/(((noOfAED + 0.5) / 4) * 100)) * 33.33).toString() + '%' : ((100/(((noOfAED - 0.5) / 4) * 100)) * 33.33).toString() + '%'
+}
+
 
 const AEDScreen = ({navigation}) => {
 
-  return (
-    <View style={styles.container}>
-        <View style={styles.subContainer}>
-           
-           <PinIcon style={styles.pin}/>
-           <PinIcon style={styles.pin}/>
-           <PinIcon style={styles.pin}/>
-           <PinIcon style={styles.pin}/>
-           <PinIcon style={styles.pin}/>
-           
-           
+    React.useEffect(() => {
+        const returnValue = pinHeight({noOfAED });
+        const b = scrollViewHeight({noOfAED })
+        console.log(returnValue)
+        console.log(b)
+    }, []);
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.subContainer}>
+                <ScrollView contentContainerStyle={styles.scrollView}>
+                    <PinIcon style={styles.pin}/>
+                    <PinIcon style={styles.pin}/>
+                    <PinIcon style={styles.pin}/>
+                    <PinIcon style={styles.pin}/>
+                    <PinIcon style={styles.pin}/>
+                    <PinIcon style={styles.pin}/>
+                    <PinIcon style={styles.pin}/>
+                </ScrollView>
+            </View>
         </View>
-    </View>
-    
-  );
+        
+    );
 };
 
 const styles = StyleSheet.create({
@@ -37,17 +55,21 @@ const styles = StyleSheet.create({
     },
     subContainer: {
         flex: 1,
-        alignItems: 'center',
-        justifyContent: 'space-evenly',
-        flexWrap: 'wrap',
-        flexDirection: 'row',
         backgroundColor: '#192734',
         height: '100%' ,
         width: '100%',
     },
+    scrollView:{
+        flexGrow: 1,
+        alignItems: 'center',
+        justifyContent: 'space-evenly',
+        flexWrap: 'wrap',
+        flexDirection: 'row',
+        height: scrollViewHeight({noOfAED })
+    },
     pin : {
         width: '50%',
-        height: '33.33%',
+        height: pinHeight({noOfAED }),
         marginBottom: '22%',
     }
    
