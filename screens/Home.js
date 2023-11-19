06309dbfeb57734aaefeb7a0fba0ec199f279fba
Aidan <aidan.lamos.2020@uni.strath.ac.uk>
2023-11-19 17:07:30 +0000

    integrated test screen into home screen

--- screens/Home.js
+++ screens/Home.js
@@ -1,19 +1,73 @@
 import React, {useState} from 'react';
-import { View, TouchableOpacity , Text, StyleSheet, TouchableHighlight, Button } from 'react-native';
+import { View, TouchableOpacity , Text, StyleSheet, Image, TouchableHighlight, Button, Dimensions } from 'react-native';
 import MapView from 'react-native-maps';
-//import externalStyle from '../style/externalStyle';
+import Modal from 'react-native-modal';
+import DownArrowIcon from '../components/down_arrow';
+import AEDImageContainer from '../components/aed_image_container';
+import LocateIcon from '../components/locate_icon';
+import ImageModal from '../components/modals/image_modal';
+
+const screenHeight = Dimensions.get('window').height
 
 const Home = ({navigation}) => {
 
-  const [modalAEDVisible, setModalAEDVisible] = useState(false);
+    const address = 'John Anderson Building \n 107 Rottenrow E \n Glasgow G4 0NG'
+    const days = 'Monday \n Tuesday \n Wednesday \n Thursday \n Friday \n Saturday \n Sunday'
+    const openingTimes = '9:00 - 17:00 \n 9:00 - 17:00 \n 9:00 - 17:00 \n 9:00 - 17:00 \n 9:00 - 17:00 \n 9:00 - 17:00 \n 9:00 - 17:00'
+  
+    const [isModalVisible, setModalVisible] = useState(false)
+    const [isImageModalVisible, setImageModalVisible] = useState(false)
+    const [animationTime, setAnimationTime] = useState(750)
+    const [needImage, setNeedImage] = useState(false)
+  
+    const toggleModal = () => {
+          setAnimationTime(750)
+          setModalVisible(!isModalVisible);
+          setNeedImage(false)
+      };
+      
+      const toggleModalWithTiming = () => {
+          setNeedImage(true)
+          setAnimationTime(1)
+          setModalVisible(!isModalVisible)
+          
+      }
+  
+      const toggleImageModal = () => {
+          if(needImage){
+              setImageModalVisible(!isImageModalVisible)
+          }
+          //setModalVisible(!isModalVisible)
+      };
 
   return (
     <View style={styles.container}>
         <MapView style={styles.map}/>
-        <TouchableOpacity style={{height: 100, width:100, backgroundColor:'#FFFFFF'}} onPress={ () => setModalAEDVisible(true)}>
-          <Text>Open</Text>
-        </TouchableOpacity>
-
+        <View style={styles.buttonContainer}>
+            <TouchableOpacity  style={styles.button} onPress={toggleModal}>
+                <Image 
+                    source={require('../assets/images/nearby.png')}
+                    resizeMode='contain'
+                    style={{height: '100%', width: '25%'}}
+                />
+                <Text style={{textAlign: 'center', color: '#FFFFFF'}}>Nearest AED</Text>
+            </TouchableOpacity>
+        </View>
+        <ImageModal visibility={isImageModalVisible} ImageModalVisibility={toggleImageModal} toggleParentModal={toggleModal}/>
+        <Modal isVisible={isModalVisible} style={styles.modal} backdropOpacity={0} swipeDirection={'down'} onSwipeComplete={() => setModalVisible(false)} animationInTiming={animationTime} animationOutTiming={animationTime} onModalHide={toggleImageModal}>
+            <View style={styles.modalView}>
+            <DownArrowIcon style={styles.downArrow} onPress={toggleModal}/>
+            <AEDImageContainer style={styles.aed} onPress={toggleModalWithTiming} />
+            <View style={styles.infoContainer}>
+                <Text style={styles.address}> {address}</Text>
+                <View style={styles.openingTimesContainer}>
+                <Text style={styles.days}> {days}</Text>
+                <Text style={styles.times}> {openingTimes}</Text>
+                </View>
+                <LocateIcon style={styles.locateButton}/>
+            </View>
+            </View>
+        </Modal>
     </View>
     
   );
@@ -23,18 +77,114 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
+        alignItems: 'flex-end',
+        flexDirection: 'row',
+        justifyContent: 'flex-end',
         backgroundColor: '#15202b',
     },
     buttonContainer: {
-        width: '100%',
-        padding: 10,
+        flex: 1,
+        justifyContent: 'center',
+        width: '55%',
+        height: '20%',
+        position: 'absolute',
+    },
+    button:{
+        alignItems: 'center',
+        justifyContent:'space-between',
+        flexDirection: 'row',
+        backgroundColor: '#018489',
+        width:'100%',
+        height: '60%',
+        borderRadius: 100,
+        paddingLeft:'10%',
+        paddingRight: '10%'
+
     },
     map: {
       height:'100%',
       width:'100%',
-    }
+    },
+
+    modal: {
+        margin: 0,
+        alignItems: 'center',
+        justifyContent: 'flex-end',
+      },
+  
+      modalView: {
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+        height: '80%',
+        width: '100%',
+        backgroundColor: '#15202b',
+        borderTopRightRadius: 50,
+        borderTopLeftRadius: 50,
+        paddingLeft: (screenHeight * 0.025),
+        paddingRight: (screenHeight * 0.025),
+        paddingBottom: (screenHeight * 0.025),
+      },
+  
+      downArrow: {
+        width: '10%',
+        aspectRatio:1
+      },
+  
+      aed: {
+        height: '25%',
+        aspectRatio: 1,
+        borderRadius: 100,
+        overflow: 'hidden',
+        borderColor: '#FFFFFF',
+        borderWidth: 5,
+        marginTop: '5%',
+        marginBottom: '5%',
+      },
+  
+      infoContainer:{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        backgroundColor: '#192734',
+        height: '100%' ,
+        width: '100%',
+      },
+  
+      locateButton:{
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: '35%',
+        height: '20%',
+        backgroundColor: '#018489',
+        marginBottom: (screenHeight * 0.025),
+        borderRadius: 32,
+       
+      },
+  
+      openingTimesContainer:{
+        width: '100%',
+        justifyContent: 'center',
+        flexDirection: 'row',
+      },
+  
+      address:{
+        textAlign:'center',
+        marginTop: (screenHeight * 0.025),
+        color: '#FFFFFF',
+        fontSize: 20,
+      },
+  
+      days:{
+        textAlign:'left',
+        color: '#FFFFFF',
+        fontSize: 20,
+      },
+  
+      times:{
+        textAlign:'right',
+        color: '#FFFFFF',
+        fontSize: 20,
+      }
 });
 
 export default Home;
