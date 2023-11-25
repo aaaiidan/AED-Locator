00fd687165f497ecfcc610016284f65b01f42de1
Aidan <aidan.lamos.2020@uni.strath.ac.uk>
2023-11-25 15:54:28 +0000

    Gesture handle working near perfect

--- screens/TestScreen.js
+++ screens/TestScreen.js
@@ -1,30 +1,51 @@
 import React, {useEffect, useState} from 'react';
-import { View, Text, StyleSheet, Dimensions, TouchableOpacity, SafeAreaView } from 'react-native';
+import { View, Text, StyleSheet, Dimensions, TouchableOpacity, SafeAreaView, Image } from 'react-native';
 import Modal from 'react-native-modal';
 import DownArrowIcon from '../components/down_arrow';
 import AEDImageContainer from '../components/aed_image_container';
 import LocateIcon from '../components/locate_icon';
-import ImageModal from '../components/modals/image_modal';
 import Animated, {  useSharedValue, useAnimatedStyle, withTiming, useAnimatedGestureHandler } from 'react-native-reanimated';
 import { PanGestureHandler } from 'react-native-gesture-handler';
   
 const screenHeight = Dimensions.get('window').height
+const screenWdidth = Dimensions.get('window').width
+const intialY = 700
+const maxY = 0
 
+const placeholder_aed = require('../assets/images/placeholder_aed.jpg')
+const image = Image.resolveAssetSource(placeholder_aed)
 
 const TestScreen = ({navigation}) => {
 
-  	const translateY = useSharedValue(700); // Initial position below the screen
-  	const gestureState = useSharedValue(0);
+  //Variables for gesture handling
+  const translateY = useSharedValue(intialY); // Initial position below the screen
+  const gestureState = useSharedValue(maxY);
+
+  //Variables for image modal
+  const [isModalVisible, setModalVisible] = useState(false);
+  const [ratio, setRatio] = useState(0);
+
 	const onGestureEvent = useAnimatedGestureHandler({
 		onStart: (_, ctx) => {
-		  	ctx.startY = translateY.value;
-		  	gestureState.value = 1;
+		  	ctx.startY = translateY.value; // ctx is object that stores phases of gesture (y value)
+		  	gestureState.value = 1; // Gesture is active 
 		},
 		onActive: (event, ctx) => {
-		  	translateY.value = ctx.startY + event.translationY;
+		  	const currentY = ctx.startY + event.translationY;
+			translateY.value = Math.min(550, Math.max(0, currentY));
+
+			if (event.velocityY > 1000) {
+				translateY.value = withTiming(intialY); // Example: Snap to maxY if the swipe velocity is high
+			} else if ( translateY.value < 350) {
+				translateY.value = withTiming(maxY);
+			} else if (translateY.value > 350) {
+				translateY.value = withTiming(intialY);
+			}
+			
 		},
 		onEnd: () => {
-		  	gestureState.value = 0;
+		  	gestureState.value = 0; // Gesture is inactive 
+
 		},
 	  });
 
@@ -39,6 +60,14 @@ const TestScreen = ({navigation}) => {
   });
 
 
+  const toggleImageModal = () => {
+    setModalVisible(!isModalVisible)
+  }
+
+  useEffect(() => {
+    setRatio(screenWdidth/image.width)
+  },[])
+
   //Placeholders
   const address = 'John Anderson Building \n 107 Rottenrow E \n Glasgow G4 0NG'
   const days = 'Monday \n Tuesday \n Wednesday \n Thursday \n Friday \n Saturday \n Sunday'
@@ -47,27 +76,42 @@ const TestScreen = ({navigation}) => {
 
 
   return (
-    <View style={styles.container}>
-      <TouchableOpacity onPress={startAnimation}  style={{ backgroundColor: 'green'}}>
-        <Text>Open</Text>
-      </TouchableOpacity>
+	<View style={styles.container}>
+		<TouchableOpacity onPress={startAnimation}  style={{ backgroundColor: 'green'}}>
+			<Text>Open</Text>
+		</TouchableOpacity>
 		<PanGestureHandler onGestureEvent={onGestureEvent}>
 			<Animated.View style={[styles.animatedView, animatedStyle]}>
+        <Modal 
+          style={styles.modalImage}
+          useNativeDriver={true}
+          animationIn='fadeIn'
+          animationOut='fadeOut'
+          isVisible={isModalVisible}
+          hideModalContentWhileAnimating
+          onBackButtonPress={toggleImageModal}
+          onBackdropPress={toggleImageModal}
+          backdropOpacity={0.9}
+        >
+          <Image 
+            source={placeholder_aed}
+            resizeMode='contain'
+            style={{ width: '100%', height: image.height * ratio}}
+          />
+        </Modal>
 				<DownArrowIcon style={styles.downArrow}/>
-				<AEDImageContainer style={styles.aed}  />
+				<AEDImageContainer style={styles.aed} onPress={toggleImageModal} />
 				<View style={styles.infoContainer}>
-					<Text style={styles.address}> {address}</Text>
-					<View style={styles.openingTimesContainer}>
-						<Text style={styles.days}> {days}</Text>
-						<Text style={styles.times}> {openingTimes}</Text>
-					</View>
-					<LocateIcon style={styles.locateButton}/>
+				  <Text style={styles.address}> {address}</Text>
+          <View style={styles.openingTimesContainer}>
+            <Text style={styles.days}> {days}</Text>
+            <Text style={styles.times}> {openingTimes}</Text>
+          </View>
+				  <LocateIcon style={styles.locateButton}/>
 				</View>
 			</Animated.View>
 		</PanGestureHandler>
-      
-      
-    </View>
+	</View>
   );
 };
 
@@ -151,7 +195,13 @@ const styles = StyleSheet.create({
       textAlign:'right',
       color: '#FFFFFF',
       fontSize: 20,
-    }
+    },
+
+    modalImage: {
+      margin: 0,
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
    
 });
 
